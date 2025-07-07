ee827c6d2d5897e22c0e75b6a2508787db50a0c0
Bayuanggoro26 <bayuanggoro098123@gmail.com>
2025-07-07 23:40:31 +0700

    merubah sedikti code

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,8 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/create', [PostController::class, 'create']);
-
 Route::post('/', [PostController::class, 'store']);
-
 Route::get('/',[PostController::class,'index']);
-
 Route::get('/{id}/edit',[PostController::class,'edit']);
- 
 Route::put('/{id}',[PostController::class,'update']);
-
 Route::delete('/{id}', [PostController::class, 'destroy']);
